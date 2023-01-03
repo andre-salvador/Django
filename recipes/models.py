ae1593ae188e502bd93bdc47964112017891f6ed
andre-salvador <andrersalvador@hotmail.com>
2023-01-03 12:44:25 -0300

    correção e tratativa de erros

--- recipes/models.py
+++ recipes/models.py
@@ -13,7 +13,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparetion_time = models.IntegerField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
@@ -22,9 +22,10 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
 
